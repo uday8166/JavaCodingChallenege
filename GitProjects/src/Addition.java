98d43fd288e72dcd7aaf1248380b9c779a2f6e59
uday8166 <Udayroyal81@gmail.com>
2022-02-16 17:53:26 +0530

    code updated

--- GitProjects/src/Addition.java
+++ GitProjects/src/Addition.java
@@ -3,5 +3,6 @@ public class Addition {
 	static int a=10, b=5;
 public static void main(String[] args) {
 System.out.println(a+b+"Sum of a & b is : ");	
+System.out.println(a-b+"Subtraction of a&b is :");
 }
 }
